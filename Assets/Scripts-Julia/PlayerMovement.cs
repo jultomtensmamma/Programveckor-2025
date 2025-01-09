5cb072292f20cebc1d304c66c8c0e60084d1cc7c
jultomtensmamma <julia.tillberg@elev.ga.lbs.se>
2025-01-09 14:53:09 +0100

    playermovement script

--- Assets/Scripts-Julia/PlayerMovement.cs
+++ Assets/Scripts-Julia/PlayerMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D myRigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.AddForce(new Vector3(0, 5, 0), ForceMode2D.Impulse);
+        }
     }
+
 }
+
+
+
