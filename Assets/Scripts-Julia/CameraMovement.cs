cd7949a9d5441bc7f822961909870fddfdd4cd10
jultomtensmamma <julia.tillberg@elev.ga.lbs.se>
2025-01-10 15:14:19 +0100

    cameramovement

--- Assets/Scripts-Julia/CameraMovement.cs
+++ Assets/Scripts-Julia/CameraMovement.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
+   
 {
+    [SerializeField]
+    GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetPosition = Player.transform.position + new Vector3(0, 0, -10);
+        transform.position = targetPosition;
     }
 }
