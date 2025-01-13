c11ebd5d3d4ae673ac607e988280a8ac1c4e222a
jultomtensmamma <julia.tillberg@elev.ga.lbs.se>
2025-01-13 14:18:12 +0100

    scripts

---
+++ Assets/Scripts-Julia/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    private bool isGrounded = true;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = 0f;
+
+        if (Input.GetKey(KeyCode.A)) horizontalInput = -1f;
+        if (Input.GetKey(KeyCode.D)) horizontalInput = 1f;
+
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
+//Jonathan kod min funkade ej
