eecabb366c7d063de76b1de223f0c53b6ce7dcd2
jultomtensmamma <julia.tillberg@elev.ga.lbs.se>
2025-01-14 10:04:22 +0100

    Nova anim

---
+++ Assets/Scripts-Julia/Scripts/AnimationController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;      // Referens till Animator-komponenten
+    private bool isRunning = false; // Bool för att hålla reda på spelarens rörelsestatus
+
+    void Start()
+    {
+        // Hämta Animator-komponenten på spelaren
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Kontrollera om spelaren rör sig (t.ex. via input)
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Om spelaren rör sig, sätt isRunning till true, annars false
+        if (Mathf.Abs(horizontalInput) > 0.20f)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+
+        // Uppdatera Animator med bool-värdet
+        animator.SetBool("isRunning", isRunning);
+    }
+}
