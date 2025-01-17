b3cd546af5991d6cea03bc5980953436000ee513
jultomtensmamma <julia.tillberg@elev.ga.lbs.se>
2025-01-17 14:13:54 +0100

    på riktigt allt möjligt

--- Assets/Scripts-Sophiya/Ability.cs
+++ Assets/Scripts-Sophiya/Ability.cs
@@ -7,13 +7,13 @@ public class Ability : MonoBehaviour
 {
     public float floatForce = 6f; // hur mkt kraft för att sväva
     public float moveSpeed = 5f; //rörelsehastighet
-    private Rigidbody2D rb; //Fysik komponent
+    private Rigidbody2D rb3; //Fysik komponent
     private bool isFloating = false;// kontrollerar ifall katten svävar
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        if (rb == null)
+        rb3 = GetComponent<Rigidbody2D>();
+        if (rb3 == null)
         {
             Debug.LogError("Rb2D saknas på objektet");
 
@@ -25,16 +25,19 @@ public class Ability : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftShift)) //ifall E tangenten trycks ned
         {
             isFloating = !isFloating; //Växla mellan sväva och inte sväva
-            rb.gravityScale = isFloating ? 0 : 1; //Växla gravitationen
+            rb3.gravityScale = isFloating ? 0 : 1; //Växla gravitationen
         }
 
          
         float moveInput = Input.GetAxis("Horizontal"); //Hanterar horisontell rörelse
-        rb.velocity = new Vector2(moveInput* moveSpeed, rb.velocity.y);
+        rb3.velocity = new Vector2(moveInput* moveSpeed, rb3.velocity.y);
 
         if (isFloating) //Uppåtriktad kraft
         {
-            rb.AddForce(Vector2.up * floatForce, ForceMode2D.Force);
+            rb3.AddForce(Vector2.up * floatForce, ForceMode2D.Force);
+            //rb.velocity = new Vector2(rb.velocity.x, floatForce);
+            //Debug.Log(rb3.velocity.y);
         }
+
     }
 }
