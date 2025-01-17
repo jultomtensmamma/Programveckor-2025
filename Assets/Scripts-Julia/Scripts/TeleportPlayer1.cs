c32f84b75c47ada71766644deb22af929fe3805b
jultomtensmamma <julia.tillberg@elev.ga.lbs.se>
2025-01-17 11:15:27 +0100

    Saving EditorBuildSettings
    All scenes added to scene list.
    
    //HCS

--- Assets/Scripts-Julia/Scripts/TeleportPlayer1.cs
+++ Assets/Scripts-Julia/Scripts/TeleportPlayer1.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // För att hantera scenbyte
 
-public class TeleportPlayer1 : MonoBehaviour
+public class TeleportPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string nextSceneName; // Namnet på nästa scen att teleportera till
+    public InventorySystem inventory; // Referens till spelarens InventorySystem
 
-    // Update is called once per frame
+    private bool canTeleport = false; // Om spelaren kan teleportera
+
     void Update()
     {
-        
+        // Kontrollera om alla komponenter har samlats in
+        if (inventory != null && inventory.hasComponent1 && inventory.hasComponent2 && inventory.hasComponent3)
+        {
+            canTeleport = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Kontrollera om spelaren nuddar teleportpunkten
+        if (other.CompareTag("Player") && canTeleport)
+        {
+            Debug.Log("Player teleported to the next scene!");
+            Teleport();
+        }
+    }
+
+    private void Teleport()
+    {
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
     }
 }
